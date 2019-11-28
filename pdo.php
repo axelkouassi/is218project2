5a924c998a2287e35cf3c0248162f66b72584162
axelgeoffroykouassi kouassi <ak659@afsconnect1.njit.edu>
2019-11-28 00:48:12 -0500

    Creation of 'pdo.php' file

---
+++ pdo.php
@@ -0,0 +1,12 @@
+<?php
+$username = 'ak659';
+$password = 'IMnYbLekv';
+$hostname = 'sql1.njit.edu';
+$dsn = "mysql:host=$hostname; dbname=$username";
+try{
+    $db = new PDO($dsn,$username, $password);
+}catch (PDOException $e){
+        echo "Connection failed: ".$e->getMessage();
+    }
+
+?>
