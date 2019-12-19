624532152d828ba89fdd120fe82eee7c3f337cb2
axelgeoffroykouassi kouassi <ak659@afsconnect1.njit.edu>
2019-12-18 19:51:52 -0500

    SQL to write input from questions.php to database

--- questions.php
+++ questions.php
@@ -7,6 +7,11 @@ $id = filter_input(INPUT_GET,'userID');
 $firstName = filter_input(INPUT_GET,'fname');
 $lastName = filter_input(INPUT_GET,'lname');
 
+// Getting input data from users on question form
+$question_name = filter_input(INPUT_POST,'question_name');
+$question_body = filter_input(INPUT_POST,'question_body');
+$question_skills = filter_input(INPUT_POST,'question_skills');
+
 ?>
 <!DOCTYPE html>
 <html lang="en-US">
@@ -52,6 +57,7 @@ $lastName = filter_input(INPUT_GET,'lname');
 
 <main>
     <form action="display_questions.php?&userID=<?php echo $id ?>&fname=<?php echo $firstName ?>&lname=<?php echo $lastName ?>" method="post" class ="form">
+
         <h1>New Question Form</h1>
         <p style="color: red; ">* Required Fields</p><br>
         <div id="login_data">
@@ -77,11 +83,39 @@ $lastName = filter_input(INPUT_GET,'lname');
             <label>&nbsp;</label>
             <input type="submit" class="btn btn-default btn-block" value ="Submit Question"><br>
         </div>
+
     </form>
 </main>
 
 </body>
 
 </html>
+
+<?php
+
+if ($id){
+    // Writing questions to database
+    // SQL Query
+    $query = 'INSERT INTO questions (ownerid, title, body, skills)
+                          VALUES (:userID, :title, :body, :skills)';
+    //Create PDO Statement
+    $statement = $db->prepare($query);
+    //Bind Form Values to SQL
+    $statement -> bindValue(':title', $question_name);
+    $statement -> bindValue(':body', $question_body);
+    $statement -> bindValue(':skills', $question_skills);
+    $statement -> bindValue(':userID', $id);
+    //Execute the SQL Query
+    $statement->execute();
+    //Close the database connection
+    $statement = closeCursor();
+
+    header("Location: display_questions.php?userID=$id%fname=$firstName&lname=$lastName");
+}
+else {
+    header('location: login.html');
+
+}
+?>
 
 
